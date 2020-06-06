455cfd884f328ffb1160a33455ced7f62f788867
SayariMio <woshigegefengleng@live.com>
2020-06-06 17:19:20 +0800

    upload code

--- robot.py
+++ robot.py
@@ -2,21 +2,22 @@ import requests
 import bs4
 
 url = 'https://github.com/zytx121/je/issues/'
-i = 1;
+i = 1
 total=''
-while i<4:
-    url = url + str(i)
-    response = requests.get(url)
+while i<1132:
+    urln = url + str(i)
+    print(urln)
+    response = requests.get(urln)
     status_code = response.status_code
 
     content = bs4.BeautifulSoup(response.content.decode("utf-8"),"lxml")
     code = content.find_all('table')
-    if(status_code == '404'): 
-        print('404')
+    if(status_code == 404): 
+        print('404_')
     else:
         print(status_code)
-        total = total + ' \t\n '+str(code[0].text.split()[0])
-        i = i + 1
+        total = total + ' \t\n OxO '+str(code[0].text)
+    i = i + 1
 
 fh = open('code.txt','a')
 fh.write(total)
