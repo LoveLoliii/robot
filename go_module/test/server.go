984865129a151f24642dec1589e26d3ad7a3ad71
loveloliii <woshigegefengleng@live.com>
2020-06-24 17:45:19 +0800

    upload file

--- go_module/test/server.go
+++ go_module/test/server.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"crypto/md5"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"os"
+	"strconv"
+	"time"
+
 	"github.com/kataras/iris/v12"
 )
 
+const maxSize = 5 << 20 //5MB
 // test new ssh key
 func main() {
 	app := iris.New()
@@ -37,8 +46,60 @@ func main() {
 		searchKey := ctx.Params().GetString("search")
 		ctx.Writef("not ready to return result by key :%s", searchKey)
 	})
+
+	// Serve the upload_form.html to the client.
+	app.Get("/upload", func(ctx iris.Context) {
+		// create a token (optionally).
+
+		now := time.Now().Unix()
+		h := md5.New()
+		io.WriteString(h, strconv.FormatInt(now, 10))
+		token := fmt.Sprintf("%x", h.Sum(nil))
+
+		// render the form with the token for any use you'd like.
+		// ctx.ViewData("", token)
+		// or add second argument to the `View` method.
+		// Token will be passed as {{.}} in the template.
+		ctx.View("upload_form.html", token)
+	})
+
+	app.Post("/upload", iris.LimitRequestBodySize(maxSize), func(ctx iris.Context) {
+		fmt.Printf("upload")
+		// Get the file from the request.
+		file, info, err := ctx.FormFile("uploadfile")
+		if err != nil {
+			ctx.StatusCode(iris.StatusInternalServerError)
+			ctx.HTML("Error while uploading: <b>" + err.Error() + "</b>")
+			return
+		}
+
+		defer file.Close()
+		fname := info.Filename
+
+		// Create a file with the same name
+		// assuming that you have a folder named 'uploads'
+		out, err := os.OpenFile("./views/upload/"+fname,
+			os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil {
+			ctx.StatusCode(iris.StatusInternalServerError)
+			ctx.HTML("Error while uploading: <b>" + err.Error() + "</b>")
+			return
+		}
+		defer out.Close()
+
+		io.Copy(out, file)
+		path := "https://sayarii.art/static/upload/" + fname
+		ctx.Writef("file path is -->:%s", path)
+	})
 	// 使用nginx监听80/443 再转发到8888
 	app.Listen(":8888")
+}
+func beforeSave(ctx iris.Context, file *multipart.FileHeader) {
+	// ip := ctx.RemoteAddr()
+	// ip = strings.Replace(ip, ".", "_", -1)
+	// ip = strings.Replace(ip, ":", "_", -1)
+	//file.Filename = ip + "-" + file.Filename
+	//fmt.Printf("fileNmae:%s", file.Filename)
 }
 func myMiddleware(ctx iris.Context) {
 	ctx.Application().Logger().Infof("Runs before %s", ctx.Path())
