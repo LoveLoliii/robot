5dcb97491bdb80f4cdb167f2595f99b3bfe3f3e3
naishan10010 <1523359063@qq.com>
2020-06-24 14:13:22 +0800

    建立go服务器

--- go_module/test/server.go
+++ go_module/test/server.go
@@ -2,41 +2,40 @@ package main
 
 import (
 	"github.com/kataras/iris/v12"
-	//"fmt"
 )
 
 func main() {
-    app := iris.Default()
-    //app.Use(myMiddleware)
-	// Load all templates from the "./views" folder
-    // where extension is ".html" and parse them
-    // using the standard `html/template` package.
-    app.RegisterView(iris.HTML("./views", ".html"))
-
-    // Method:    GET
-    // Resource:  http://localhost:8080
+	app := iris.New()
+	app.RegisterView(iris.HTML("./views", ".html"))
+	//app.Use(myMiddleware)
+	// Method:    GET
+	// Resource:  http://localhost:8080
 	app.Get("/", func(ctx iris.Context) {
-        // Bind: {{.message}} with "Hello world!"
-        ctx.ViewData("message", "Hello world!")
-        // Render template file: ./views/hello.html
-        ctx.View("hello.html")
+		// Bind: {{.message}} with "Hello world!"
+		ctx.ViewData("message", "Hello world!")
+		// Render template file: ./views/hello.html
+		ctx.View("hello.html")
 	})
-	
-    app.Handle("GET", "/ping", func(ctx iris.Context) {
-        ctx.JSON(iris.Map{"message": "pong"})
-    })
+
+	// app.Get("/", func(ctx iris.Context) {
+	// 	ctx.Writef("Hello from the SECURE server")
+	// })
+
+	app.Get("/mypath", func(ctx iris.Context) {
+		ctx.Writef("Hello from the SECURE server on path /mypath")
+	})
+	app.Handle("GET", "/ping", func(ctx iris.Context) {
+		ctx.JSON(iris.Map{"message": "pong"})
+	})
 
 	app.Get("/user/{id:uint64}", func(ctx iris.Context) {
-        userID, _ := ctx.Params().GetUint64("id")
-        ctx.Writef("User ID: %d", userID)
-    })
-
-    // Listens and serves incoming http requests
-    // on http://localhost:8080.
-    app.Listen(":80")
+		userID, _ := ctx.Params().GetUint64("id")
+		ctx.Writef("User ID: %d", userID)
+	})
+	// 使用nginx监听80/443 再转发到8888
+	app.Listen(":8888")
 }
-
 func myMiddleware(ctx iris.Context) {
-    ctx.Application().Logger().Infof("Runs before %s", ctx.Path())
-    ctx.Next()
+	ctx.Application().Logger().Infof("Runs before %s", ctx.Path())
+	ctx.Next()
 }
