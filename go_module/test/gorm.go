ecbe5e3a6396449014da00ab3dfcfda3548a43b2
loveloliii <woshigegefengleng@live.com>
2020-06-29 13:32:37 +0800

    合并

--- go_module/test/gorm.go
+++ go_module/test/gorm.go
@@ -1,56 +1,56 @@
-package main
+// package main
 
-import (
-	"fmt"
+// import (
+// 	"fmt"
 
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
-)
+// 	"github.com/jinzhu/gorm"
+// 	_ "github.com/jinzhu/gorm/dialects/sqlite"
+// )
 
-type Product struct {
-	gorm.Model
-	Code  string
-	Price uint
-}
-type Song struct {
-	Title  string
-	Pic    string
-	Singer string
-	Score  string
-	gorm.Model
-}
+// type Product struct {
+// 	gorm.Model
+// 	Code  string
+// 	Price uint
+// }
+// type Song struct {
+// 	Title  string
+// 	Pic    string
+// 	Singer string
+// 	Score  string
+// 	gorm.Model
+// }
 
-func getSong(t string) Song {
-	db, err := gorm.Open("sqlite3", "song.db")
-	if err != nil {
-		panic("failed to connect database")
-	}
-	defer db.Close()
+// func getSong(t string) Song {
+// 	db, err := gorm.Open("sqlite3", "song.db")
+// 	if err != nil {
+// 		panic("failed to connect database")
+// 	}
+// 	defer db.Close()
 
-	db.AutoMigrate(&Song{})
-	var ss Song
-	//db.First(&product, 1)
-	db.First(&ss, "title = ?", t)
-	return ss
-}
-func addSong(s Song) {
-	db, err := gorm.Open("sqlite3", "song.db")
-	if err != nil {
-		panic("failed to connect database")
-	}
-	defer db.Close()
+// 	db.AutoMigrate(&Song{})
+// 	var ss Song
+// 	//db.First(&product, 1)
+// 	db.First(&ss, "title = ?", t)
+// 	return ss
+// }
+// func addSong(s Song) {
+// 	db, err := gorm.Open("sqlite3", "song.db")
+// 	if err != nil {
+// 		panic("failed to connect database")
+// 	}
+// 	defer db.Close()
 
-	db.AutoMigrate(&Song{})
+// 	db.AutoMigrate(&Song{})
 
-	db.Create(&Song{Title: s.Title, Pic: s.Pic,
-		Singer: s.Singer, Score: s.Score})
+// 	db.Create(&Song{Title: s.Title, Pic: s.Pic,
+// 		Singer: s.Singer, Score: s.Score})
 
-	var ss Song
-	//db.First(&product, 1)
-	db.First(&ss, "title = ?", "支え")
-	fmt.Printf(ss.Singer)
-	//db.Model(&product).Update("Price", 2000)
+// 	var ss Song
+// 	//db.First(&product, 1)
+// 	db.First(&ss, "title = ?", "支え")
+// 	fmt.Printf(ss.Singer)
+// 	//db.Model(&product).Update("Price", 2000)
 
-	//db.Delete(&product)
+// 	//db.Delete(&product)
 
-}
+// }
--- go_module/test/server.go
+++ go_module/test/server.go
@@ -10,6 +10,10 @@ import (
 	"time"
 
 	"github.com/kataras/iris/v12"
+
+	//gorm
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 const maxSize = 5 << 20 //5MB
@@ -123,4 +127,53 @@ func beforeSave(ctx iris.Context, file *multipart.FileHeader) {
 func myMiddleware(ctx iris.Context) {
 	ctx.Application().Logger().Infof("Runs before %s", ctx.Path())
 	ctx.Next()
+}
+
+//gorm
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
+type Song struct {
+	Title  string
+	Pic    string
+	Singer string
+	Score  string
+	gorm.Model
+}
+
+func getSong(t string) Song {
+	db, err := gorm.Open("sqlite3", "song.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	db.AutoMigrate(&Song{})
+	var ss Song
+	//db.First(&product, 1)
+	db.First(&ss, "title = ?", t)
+	return ss
+}
+func addSong(s Song) {
+	db, err := gorm.Open("sqlite3", "song.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	db.AutoMigrate(&Song{})
+
+	db.Create(&Song{Title: s.Title, Pic: s.Pic,
+		Singer: s.Singer, Score: s.Score})
+
+	var ss Song
+	//db.First(&product, 1)
+	db.First(&ss, "title = ?", "支え")
+	fmt.Printf(ss.Singer)
+	//db.Model(&product).Update("Price", 2000)
+
+	//db.Delete(&product)
+
 }
